ddf3d6bc474e1542e8d4f91e7f1437da8d9f308b
Balev,Kristiyan K.E <k.balev@student.fontys.nl>
2021-10-07 18:10:35 +0200

    adding movies

--- individual-project-front-end/src/App.js
+++ individual-project-front-end/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get("http://localhost:8080/movies")
+        .then((res) => {
+          if (res.data.error) {
+            console.log(res.data);
+            alert("Something went wrong");
+          } else {
+            setMovies(res.data);
+          }
+        });
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+      <div>
+      {movies && (
+        <div className="movies">
+
+          {movies.map((movie, index) => (
+            <div key={index}>
+              <h2>{movie.title}</h2>
+            </div>
+          ))}
+
+
+        </div>
+      )}
+      </div>
     </div>
   );
 }
