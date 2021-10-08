de2440a17ac3fadf2a4b0ae4250b248342780eae
Balev,Kristiyan K.E <k.balev@student.fontys.nl>
2021-10-08 23:01:41 +0200

    added movies displayed in flexbox

--- individual-project-front-end/src/App.js
+++ individual-project-front-end/src/App.js
@@ -3,28 +3,8 @@ import { BrowserRouter as Router, Switch, Route, useHistory } from "react-router
 
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [movies, setMovies] = useState(null);
-
-  useEffect(() => {
-    const getData = () => {
-      axios
-        .get("http://localhost:8080/movies")
-        .then((res) => {
-          if (res.data.error) {
-            console.log(res.data);
-            alert("Something went wrong");
-          } else {
-            setMovies(res.data);
-          }
-        });
-    }
-    getData();
-  }, []);
-
   return (
     <div className="App">
       <Router>
@@ -33,20 +13,6 @@ function App() {
           <Route path='/' exact component={Home} />
         </Switch>
       </Router>
-      <div>
-      {movies && (
-        <div className="movies">
-
-          {movies.map((movie, index) => (
-            <div key={index}>
-              <h2>{movie.title}</h2>
-            </div>
-          ))}
-
-
-        </div>
-      )}
-      </div>
     </div>
   );
 }
--- individual-project-front-end/src/pages/Home.js
+++ individual-project-front-end/src/pages/Home.js
@@ -1,8 +1,45 @@
 import './Home.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
-    return(
-        <div class="home-page">Home page</div>
+    const [movies, setMovies] = useState(null);
+
+    useEffect(() => {
+        const getData = () => {
+            axios
+                .get("http://localhost:8080/movies")
+                .then((res) => {
+                    if (res.data.error) {
+                        console.log(res.data);
+                        alert("Something went wrong");
+                    } else {
+                        setMovies(res.data);
+                    }
+                });
+        }
+        getData();
+    }, []);
+    return (
+        <div class="home-page">Home page
+            <div>
+                {movies && (
+                    <div className="movies">
+
+                        {movies.map((movie, index) => (
+                            <div className="movie-card" key={index}>
+                                <a href=""/>
+                                <div className="movie-photo">
+                                    <p>Movie photo</p>
+                                </div>
+                                <h2>{movie.title}</h2>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+
     );
 }
 
