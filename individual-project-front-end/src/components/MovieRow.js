bacc1d42ec87b992b6085cf6a389cb0deabf0621
Balev,Kristiyan K.E <k.balev@student.fontys.nl>
2021-10-14 21:46:04 +0200

    component update

--- individual-project-front-end/src/components/MovieRow.js
+++ individual-project-front-end/src/components/MovieRow.js
@@ -2,6 +2,7 @@ import './MovieRow.css'
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, useHistory, Link } from "react-router-dom";
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 function MovieRow() {
     const [movies, setMovies] = useState(null);
@@ -27,15 +28,8 @@ function MovieRow() {
                 {movies && (
                     <div className="movies">
 
-                        {movies.map((movie, index) => (
-                            <div className="movie-card" key={index}>
-                                <Link to={"/movies/" + movie.id}>
-                                    <div className="movie-photo">
-                                        <p>Movie photo</p>
-                                    </div>
-                                    <h2>{movie.title}</h2>
-                                </Link>
-                            </div>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} key={movie.id}/>
                         ))}
                     </div>
                 )}
